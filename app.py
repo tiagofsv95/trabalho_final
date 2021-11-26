dd87d1d58f700659d86ae76231ab87c3cf1e4465
Tiago Felipe Sanches <tiagofsv95@gmail.com>
2021-11-26 19:05:42 -0300

    commit all changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_response, send_from_directory
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 import os
 import breed_routes
